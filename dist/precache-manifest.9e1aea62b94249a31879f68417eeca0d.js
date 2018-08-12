bdbc6a48d3c0944f6eb1e8f115d09ab9d6060d52
Florent Cima <669433+florentcm@users.noreply.github.com>
2018-08-12 00:42:32 -0400

    Initial dist subtree commit

---
+++ dist/precache-manifest.9e1aea62b94249a31879f68417eeca0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0adba254d5690c765b5a",
+    "url": "/js/chunk-vendors.67fbfee1.js"
+  },
+  {
+    "revision": "d219bf1951f49b36b493",
+    "url": "/js/app.54b9d1c2.js"
+  },
+  {
+    "revision": "5351a9bfef9b1c891d5a70b48728834a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0adba254d5690c765b5a",
+    "url": "/css/chunk-vendors.fbd60757.css"
+  },
+  {
+    "revision": "d219bf1951f49b36b493",
+    "url": "/css/app.def9a916.css"
+  }
+];
