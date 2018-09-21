b621477fdaa0734d3bd0b35db4ee9284304440c9
Florent Cima <florentcima@Florents-MacBook-Pro.local>
2018-09-20 21:46:05 -0400

    New build

---
+++ dist/precache-manifest.00320826d0b4d466a2452da71a201d8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d13296c52431cb090c16",
+    "url": "/js/chunk-vendors.53f042f2.js"
+  },
+  {
+    "revision": "b6e800f9c19d42fb0e0c",
+    "url": "/js/app.4730747d.js"
+  },
+  {
+    "revision": "471f4745049fbb1cc8fd9be4ed12814a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d13296c52431cb090c16",
+    "url": "/css/chunk-vendors.fbd60757.css"
+  },
+  {
+    "revision": "b6e800f9c19d42fb0e0c",
+    "url": "/css/app.a392ea64.css"
+  }
+];
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e1aea62b94249a31879f68417eeca0d.js"
+  "/precache-manifest.00320826d0b4d466a2452da71a201d8b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "florentcima"});
