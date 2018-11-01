3eb3d14083257ed58180544de980d3d1ac8d1bb8
Florent Cima <florentcima@Florents-MacBook-Pro.local>
2018-10-31 23:18:50 -0400

    Change of the deploy script

--- README.md
+++ README.md
@@ -19,3 +19,18 @@ npm run build
 ```
 npm run lint
 ```
+
+### Setup of the git worktree
+
+$ rm -rf dist
+$ echo "dist/" >> .gitignore
+$ git worktree add dist gh-pages
+
+### Deploying changes
+
+$ make # or what ever you run to populate dist
+$ cd dist
+$ git add --all
+$ git commit -m "Deploy to gh-pages"
+$ git push origin gh-pages
+$ cd ..
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-git commit -am "Save uncommited changes (WIP)"
-git branch --delete --force gh-pages
-git checkout --orphan gh-pages
-git add -f dist
-git commit -m "Rebuild GitHub pages"
-git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages && git checkout master
+cd dist
+git add --all
+git commit -m "Deploy to gh-pages"
+git push origin gh-pages
+cd ..
